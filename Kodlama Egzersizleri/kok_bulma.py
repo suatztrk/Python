f4532de42bcade1af9a0f799f18e59c689d69983
Macır <*suat.ozturk94@gmail.com>
2020-03-27 14:28:53 +0300

    kok_bulma düzeltme

--- Kodlama Egzersizleri/kok_bulma.py
+++ Kodlama Egzersizleri/kok_bulma.py
@@ -11,7 +11,7 @@ a = int(input("a:"))
 b = int(input("b:"))
 c = int(input("c:"))
 
-delta = b ** 2 - 4 * a * b * c
+delta = b ** 2 - 4 * a * c
 
 x1 = (-b - delta ** 0.5) / (2 * a)
 x2 = (-b + delta ** 0.5) / (2 * a)
