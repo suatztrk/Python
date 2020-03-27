37befd96892601c03a8156ed6713688a657429b0
Macır <*suat.ozturk94@gmail.com>
2020-03-27 13:46:56 +0300

    kodlama egzersizleri step1

---
+++ Kodlama Egzersizleri/oyuncu_kaydetme.py
@@ -0,0 +1,12 @@
+print("Oyuncu Kaydetme Programı")
+ad = input("Oyuncunun Adı : ")
+soyad = input("Oyuncunun Soyadı : ")
+takim = input("Oyuncunun Takımı : ")
+
+
+
+bilgiler=[ad, soyad, takim]
+print("Bilgiler Kaydediliyor...")
+
+print("Oyuncu Adı : {}\nOyuncu Soyadı : {}\nOyuncunun Takımı :  {}\n".format(bilgiler[0], bilgiler[1], bilgiler[2]))
+print("Bilgiler kaydedildi...")
