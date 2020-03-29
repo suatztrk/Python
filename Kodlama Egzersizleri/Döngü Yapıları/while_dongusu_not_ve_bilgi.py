9fe0d861f2c335bcbacef8d580b0223bd002fd8c
Macır <*suat.ozturk94@gmail.com>
2020-03-29 23:29:20 +0300

    While döngüsü not ve bilgiler

---
+++ Kodlama Egzersizleri/Döngü Yapıları/while_dongusu_not_ve_bilgi.py
@@ -0,0 +1,21 @@
+#while dönügü false olana kadar çalışmaya devam eder.
+"""
+i = 0
+while (i < 10):
+    print("i nin değeri : ", i)
+    i += 1
+
+a = 0
+while (a <40 ):
+    print("Python Öğreniyorum")
+    a += 1
+"""
+liste = [1, 2, 3, 4, 5]
+for i in liste:
+    print(i)
+index = 0
+while (index < len(liste)):
+    print("İndex :", index, "Liste Elemanı :", liste[index])
+    index += 1
+#Not : Eğer bir listede dönüyorsak for döngüsünü kullanmak daha garanti olacaktır.Çünkü while döngüsünde arttımayı unuttuğumuz zaman ki
+#€unutabiliriz sonsuza kadar gider.For döngüsünde işimiz daha kolay olacağından kullanmak daha mantıklı gibi görülüyor.
